5851958c6e15793a954994fd8a829d32ef3a1ed9
Ted Ian Osias <osiastedian@gmail.com>
2024-05-07 23:25:53 +0800

    fix: generate providers on run time

--- src/utils/eth.ts
+++ src/utils/eth.ts
@@ -5,37 +5,38 @@ import fs from "fs";
 export const RPC_URL = process.env.RPC_URL || "http://localhost:8545";
 export const CHAIN_ID = process.env.CHAIN_ID || "31337";
 export const CHAIN_NAME = process.env.CHAIN_NAME || "hard-hat";
-export const provider = new ethers.JsonRpcProvider(RPC_URL, {
-  chainId: parseInt(CHAIN_ID),
-  name: CHAIN_NAME,
-});
+
 export const signerPrivateKey = process.env.PRIVATE_KEY!;
 export const FUND_DISTRIBUTOR_ADDRESS = process.env.FUND_DISTRIBUTOR_ADDRESS!;
 export const FUND_DISTRIBUTOR_ABI_FILE_PATH =
   process.env.FUND_DISTRIBUTOR_ABI_FILE_PATH || "./abi.json";
-
-const signer = new ethers.Wallet(signerPrivateKey, provider);
 
 if (fs.existsSync(FUND_DISTRIBUTOR_ABI_FILE_PATH) === false) {
   console.error("FUND_DISTRIBUTOR_ABI_FILE_PATH is not set");
   process.exit(1);
 }
 
-const abiRaw = fs.readFileSync(FUND_DISTRIBUTOR_ABI_FILE_PATH).toString();
-const { abi: abiJson } = JSON.parse(abiRaw);
+const getFundDistributorContract = () => {
+  const provider = new ethers.JsonRpcProvider(RPC_URL, {
+    chainId: parseInt(CHAIN_ID),
+    name: CHAIN_NAME,
+  });
 
-const abi = new Interface(abiJson);
-const fundDistributorContract = new ethers.Contract(
-  FUND_DISTRIBUTOR_ADDRESS,
-  abi,
-  signer
-);
+  const signer = new ethers.Wallet(signerPrivateKey, provider);
+
+  const abiRaw = fs.readFileSync(FUND_DISTRIBUTOR_ABI_FILE_PATH).toString();
+  const { abi: abiJson } = JSON.parse(abiRaw);
+
+  const abi = new Interface(abiJson);
+  return new ethers.Contract(FUND_DISTRIBUTOR_ADDRESS, abi, signer);
+};
 
 export const registerAccount = async (
   depositAddress: string,
   recipientAddress: string,
   signedMessage: string
 ): Promise<string | null> => {
+  const fundDistributorContract = getFundDistributorContract();
   const call: TransactionResponse =
     await fundDistributorContract.registerAccount(
       recipientAddress,
