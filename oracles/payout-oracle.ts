e703820307c160bcf88bc8f3bbbce1d7f063a598
Ted Ian Osias <osiastedian@gmail.com>
2024-05-10 13:09:54 +0800

    refactor payout oracle

--- oracles/payout-oracle.ts
+++ oracles/payout-oracle.ts
@@ -2,41 +2,9 @@ import dotenv from "dotenv";
 import { ContractTransactionResponse, ethers } from "ethers";
 import { getTransaction } from "./blockbook-utils";
 import { wallet, fundDistributorContract } from "./contract-utils";
-import fs from "fs";
 
 dotenv.config();
 
-const RPC_URL = process.env.RPC_URL;
-const PAYOUT_REGISTRAR_PRIVATE_KEY = process.env.PAYOUT_REGISTRAR_PRIVATE_KEY;
-const FUND_DISTRIBUTOR_ADDRESS = process.env.FUND_DISTRIBUTOR_ADDRESS;
-const FUND_DISTRIBUTOR_ABI_FILE_PATH =
-  process.env.FUND_DISTRIBUTOR_ABI_FILE_PATH ?? "./abi.json";
-
-console.log("Payout Constants", {
-  RPC_URL,
-  PAYOUT_REGISTRAR_PRIVATE_KEY,
-  FUND_DISTRIBUTOR_ADDRESS,
-});
-
-if (RPC_URL === undefined) {
-  console.error("RPC_URL is not set");
-  process.exit(1);
-}
-
-if (!PAYOUT_REGISTRAR_PRIVATE_KEY) {
-  console.error("PAYOUT_REGISTRAR_PRIVATE_KEY is not set");
-  process.exit(1);
-}
-
-if (!FUND_DISTRIBUTOR_ADDRESS) {
-  console.error("FUND_DISTRIBUTOR_ADDRESS is not set");
-  process.exit(1);
-}
-
-if (fs.existsSync(FUND_DISTRIBUTOR_ABI_FILE_PATH) === false) {
-  console.error("FUND_DISTRIBUTOR_ABI_FILE_PATH is not set");
-  process.exit(1);
-}
 const BITCOIN_IN_SATOSHI = 100_000_000;
 
 console.log("Payout Oracle is running");
@@ -73,7 +41,8 @@ const run = async () => {
           return;
         }
 
-        const call: ContractTransactionResponse = await fundDistributorContract.payout(txId);
+        const call: ContractTransactionResponse =
+          await fundDistributorContract.payout(txId);
         call.wait(1).then(() => {
           console.log("Payout completed", [txId, amountInSats]);
         });
