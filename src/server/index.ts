4865ee899cd3d8967e45c684cfef6d2cfc49b2b6
Ted Ian Osias <osiastedian@gmail.com>
2024-05-10 13:43:51 +0800

    add health endpoint

--- src/server/index.ts
+++ src/server/index.ts
@@ -11,6 +11,10 @@ app.use(express.json());
 
 app.use("/api/account", accountRouter);
 
+app.get("/health", (req, res) => {
+  res.status(200).send("OK");
+});
+
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
